e598e4640541cebb35326a459a5175ca796829f5
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-03 16:29:58 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// // let a = window
+// // console.log(a);
+
+// window.setTimeout(function(){
+//     window.location.href = "https://airforshare.com/"
+// },4000)
+
+// window.setTimeout(function(){
+//     window.open("https://airforshare.com/", "_blank")
+// },4000)
+
+// let a = window.setInterval(function(){
+//     window.open("https://airforshare.com/", "_blank")
+// },4000)
+
+// clearInterval(a)
